cd85df6e3c926cd3041263347667ad129c84b2df
eddiefrs <eddie@flexrentalsolutions.com>
2019-07-11 13:19:18 -0500

    test queryloader fix for firefox

---
+++ precache-manifest.fc7e851a1f76c5c160f79533431bd5a4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c207c0a75c1aca30eb11063c53f8234",
+    "url": "/amarillohop/index.html"
+  },
+  {
+    "revision": "75f9d11504a330a8afca",
+    "url": "/amarillohop/static/css/main.1477eebc.chunk.css"
+  },
+  {
+    "revision": "a161419bc58a393b77fb",
+    "url": "/amarillohop/static/js/2.5050e4a4.chunk.js"
+  },
+  {
+    "revision": "75f9d11504a330a8afca",
+    "url": "/amarillohop/static/js/main.0d28b49c.chunk.js"
+  },
+  {
+    "revision": "1395d70a260b87c4d2b2",
+    "url": "/amarillohop/static/js/runtime~main.cd2670cb.js"
+  },
+  {
+    "revision": "510678164b97555cbea86258214e1438",
+    "url": "/amarillohop/static/media/HomepageBG1_3-min.51067816.png"
+  },
+  {
+    "revision": "73835e9aa0f68a81e46d5b38a4e8363a",
+    "url": "/amarillohop/static/media/HomepageBG2_2.73835e9a.png"
+  },
+  {
+    "revision": "670a8537d8a0688fe1a33c88b5b6491c",
+    "url": "/amarillohop/static/media/SiteBG.670a8537.jpg"
+  },
+  {
+    "revision": "0ee2847a8747345362b33b795b84e4a3",
+    "url": "/amarillohop/static/media/SiteLogo.0ee2847a.png"
+  },
+  {
+    "revision": "98910bc872521ef4f228dbe42958c825",
+    "url": "/amarillohop/static/media/bottle_me_1.98910bc8.webm"
+  }
+]);
